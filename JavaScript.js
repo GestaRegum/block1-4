c49ebe305ed7803657676fda376ef6ab9bd51b73
GestaRegum <147824834+GestaRegum@users.noreply.github.com>
2024-02-29 15:12:22 +0300

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,19 @@
+let items = document.querySelectorAll('.item');
+let open = document.querySelector('.open');
+
+
+
+
+open.addEventListener('click', function(evn) {
+    evn.preventDefault();
+
+    items.forEach(item => 
+        item.classList.toggle('hideItemLT'));
+
+        items.forEach(item =>    
+        item.classList.toggle('hideItemDT'));
+
+        open.classList.toggle('rotate');
+
+});
+
